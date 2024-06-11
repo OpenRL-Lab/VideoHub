f02dd5dd3e6de32d432c9b5c751870e111680b4a
huangshiyu <shiyu.huang@aminer.cn>
2024-06-11 21:12:23 +0800

    update

--- examples/test_api.py
+++ examples/test_api.py
@@ -1,16 +1,10 @@
 from videohub import Client
-
-
-def main():
-    client = Client(base_url="http://172.19.128.69:12345/video_qa", api_key="sk-")
-    print(
-        client.generate(
-            model="CogVLM",
-            video_path="../test_data/ikun.mp4",
-            prompt="When does the man begin to play basketball?",
-        )
+# CogVLM无需api_key，需要其他模型的秘钥请联系黄世宇
+client = Client(base_url="http://172.19.128.69:12345/video_qa", api_key="sk-")
+response = client.generate(
+        model="CogVLM",
+        video_path="../test_data/ikun.mp4",
+        prompt="When does the man begin to play basketball?",
+        # prompt="do you like play basketball?",
     )
-
-
-if __name__ == "__main__":
-    main()
+print(response)
