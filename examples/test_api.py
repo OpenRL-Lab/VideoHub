ad2be646e981275e74a45e46b6b55998d9ccbfb0
huangshiyu <shiyu.huang@aminer.cn>
2024-07-04 18:37:24 +0800

    update

--- examples/test_api.py
+++ examples/test_api.py
@@ -1,10 +1,10 @@
 from videohub import Client
 client = Client(base_url="http://172.19.128.69:12345/video_qa", api_key="sk-")
 response = client.generate(
-        model="CogVLM",
+        model="CogVLM2-Video",
         video_path="../test_data/ikun.mp4",
         # prompt="When does the man begin to play basketball?",
         prompt="How old are you?",
-        temperature=0.5,
+        temperature=0.1,
     )
 print(response)
